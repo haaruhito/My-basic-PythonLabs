547ea2729187f753655ca6d3ee87bbbe30aa7416
shree <harrynepal@gmail.com>
2020-06-05 13:51:02 +0300

    Added new files

---
+++ FindingLargestElementInList.py
@@ -0,0 +1,7 @@
+myList = [10, 8, 6, 4, 2]
+newList = myList[1:-1] #list starts at index[1] and ends at index[-1] where [-1] is not included
+print(newList)
+
+AnotherList = [10, 8, 6, 4, 2]
+AnothernewList = AnotherList[-1:1]
+print(AnothernewList) #output will be an empty list.
